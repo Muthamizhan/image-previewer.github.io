9273d7afdbb163b70357403a358987ec583caac2
Muthamizhan <msmuthu008@gmail.com>
2015-06-12 12:53:14 +0530

    facebook page for desktop version

--- app.js
+++ app.js
@@ -2,16 +2,17 @@
 
 var app = angular.module('StarterApp', ['ngMaterial']);
 
-app.controller('AppCtrl', ['$scope', '$mdSidenav',
+ app.controller('AppCtrl', ['$scope', '$mdSidenav',
     function($scope, $mdSidenav) {
         $scope.toggleSidenav = function(menuId) {
             $mdSidenav(menuId).toggle();
         };
-        
+
 
         this.tab = 1;
         this.selectTab = function(setTab) {
             this.tab = setTab;
+            setTimeout(function(){ $mdSidenav('left').close(); },300);
         };
         this.isSelected = function(checkTab) {
             return this.tab === checkTab;
